35754e22af336b4daa25aa5a586f11a1d84722ff
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-18 21:43:49 +0900

    개

---
+++ src/main/java/com/codingtest/chs/class1/BOJ_10172.java
@@ -0,0 +1,11 @@
+package main.java.com.codingtest.chs.class1;
+
+public class BOJ_10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
