428422cc480654edd9b55504896e95e1f91d98dc
nare20027 <nare20027@gmail.com>
2025-02-18 21:52:47 +0900

    10950 A+B - 3

---
+++ src/main/java/com/codingtest/ksw/class1/BOJ_10950.java
@@ -0,0 +1,27 @@
+package src.main.java.com.codingtest.ksw.class1;
+
+import java.util.Scanner;
+
+public class BOJ_10950 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int index = sc.nextInt();
+
+        int arr[] = new int[index];
+
+        for(int i = 0; i< index; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = a + b;
+        }
+
+        for(int j : arr) {
+            System.out.println(j);
+        }
+
+    }
+
+}
