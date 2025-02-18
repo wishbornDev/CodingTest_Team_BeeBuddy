7fadc0b8df52356ef4c2cf0179ba1331478b87de
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-02-18 22:18:13 +0900

    9498번 풀이 완료

---
+++ src/main/java/com/codingtest/kjw/class1/java/src/main/java/boj09498/Main.java
@@ -0,0 +1,27 @@
+package boj09498;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+        System.out.println(grade(score));
+    }
+
+    public static char grade(int score) {
+        if (score >= 90 && score <= 100) {
+            return 'A';
+        }
+        if (score >= 80 && score < 90) {
+            return 'B';
+        }
+        if (score >= 70 && score < 80) {
+            return 'C';
+        }
+        if (score >= 60 && score < 70) {
+            return 'D';
+        }
+        return 'F';
+    }
+}
