5b2ff7ff9dda600dcf2c853778c07875fa921a8c
nare20027 <nare20027@gmail.com>
2025-02-18 21:51:45 +0900

    9498 시험 성적

---
+++ src/main/java/com/codingtest/ksw/class1/BOJ_9498.java
@@ -0,0 +1,27 @@
+package src.main.java.com.codingtest.ksw.class1;
+
+import java.util.Scanner;
+
+public class BOJ_9498 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        num = sc.nextInt();
+
+        if(num >= 90) {
+            System.out.println('A');
+        }else if(num >= 80) {
+            System.out.println('B');
+        }else if(num >= 70) {
+            System.out.println('C');
+        }else if(num >= 60) {
+            System.out.println('D');
+        }else {
+            System.out.println('F');
+        }
+
+    }
+}
