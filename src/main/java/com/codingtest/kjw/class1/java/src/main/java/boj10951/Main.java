69492e0203e8eb3a02722fb9e6638a5ea480a707
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-02-20 00:25:41 +0900

    10951번 풀이 완료

---
+++ src/main/java/com/codingtest/kjw/class1/java/src/main/java/boj10951/Main.java
@@ -0,0 +1,24 @@
+package boj10951;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while (true) {
+            input = br.readLine();
+            if (input == null) { //input.equals("")
+                break;
+            }
+            String[] ints = input.split(" ");
+            int first = Integer.parseInt(ints[0]);
+            int second = Integer.parseInt(ints[1]);
+            bw.write(Integer.toString(first + second));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
