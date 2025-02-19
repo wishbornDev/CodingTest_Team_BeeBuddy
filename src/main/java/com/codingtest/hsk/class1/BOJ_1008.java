cbc982870822fb986ceb9bd4f134baab66e3a529
dxmlk <1101sky@korea.ac.kr>
2025-02-19 15:57:23 +0900

    A/B

---
+++ src/main/java/com/codingtest/hsk/class1/BOJ_1008.java
@@ -0,0 +1,20 @@
+package main.java.com.codingtest.hsk.class1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1008 {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A/B);
+    }
+    public static void main(String[] args) throws Exception {
+        new BOJ_1008().solution();
+    }
+}
