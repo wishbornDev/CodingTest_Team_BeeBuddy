87ff8c196eef2a292aa21e8cd7eed037f41af187
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-02-22 13:52:34 +0900

    2577번 풀이 완료

---
+++ src/main/java/com/codingtest/kjw/class1/java/src/main/java/boj02577/Main.java
@@ -0,0 +1,24 @@
+package boj02577;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int third = sc.nextInt();
+
+        int mul = first * second * third;
+        String convertedMul = Integer.toString(mul);
+        for (int i = 0; i < 10; i++) {
+            int count = 0;
+            for (int j = 0; j < convertedMul.length(); j++) {
+                if (convertedMul.charAt(j) == Integer.toString(i).charAt(0)) {
+                    count += 1;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
