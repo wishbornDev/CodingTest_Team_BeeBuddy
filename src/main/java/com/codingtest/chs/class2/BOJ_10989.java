3aa3ebd41a9cf5776923cbe1973be36419018eea
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-28 23:59:59 +0900

    수 정렬하기 3

--- src/main/java/com/codingtest/chs/class2/BOJ_10989.java
+++ src/main/java/com/codingtest/chs/class2/BOJ_10989.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class BOJ_10989 {
     public static void main(String[] args) throws IOException {
