3fb1a3b1f1963d36359138ec5994c821b63b1641
Cho-Hyun-Seung <toki0327@naver.com>
2025-02-28 19:01:32 +0900

    달팽이는 올라가고 싶다

---
+++ src/main/java/com/codingtest/chs/class2/BOJ_2869.java
@@ -0,0 +1,19 @@
+package main.java.com.codingtest.chs.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int up = Integer.parseInt(st.nextToken());
+        int down = Integer.parseInt(st.nextToken());
+        int height = Integer.parseInt(st.nextToken());
+        int day = 0;
+        day = (int)Math.ceil((double)(height - down) / (up - down));
+        System.out.println(day);
+    }
+}
