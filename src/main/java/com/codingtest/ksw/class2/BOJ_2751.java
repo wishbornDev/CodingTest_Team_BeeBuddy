b0045032dba15f599ff4af1d814d364e6c25f809
nare20027 <nare20027@gmail.com>
2025-03-03 18:28:00 +0900

    BOJ_2751 수 정렬하기 2

---
+++ src/main/java/com/codingtest/ksw/class2/BOJ_2751.java
@@ -0,0 +1,34 @@
+package src.main.java.com.codingtest.ksw.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class BOJ_2751 {
+    public static void main(String[] args) throws IOException {
+        /*
+         * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.*/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Set<Integer> set = new TreeSet<>();
+
+        while(n > 0) {
+            set.add(Integer.parseInt(br.readLine()));
+            n--;
+        }
+
+        Iterator<Integer> iter = set.iterator();
+
+        while(iter.hasNext()) {
+            sb.append(iter.next()).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
