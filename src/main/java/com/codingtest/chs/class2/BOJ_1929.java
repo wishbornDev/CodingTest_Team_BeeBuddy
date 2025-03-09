a4c33f7ffe2a8f07f269e1b4460102f0f0248a72
Cho-Hyun-Seung <toki0327@naver.com>
2025-03-09 19:44:16 +0900

    소수 구하기 - 시간 초과 해결

--- src/main/java/com/codingtest/chs/class2/BOJ_1929.java
+++ src/main/java/com/codingtest/chs/class2/BOJ_1929.java
@@ -3,26 +3,46 @@ package main.java.com.codingtest.chs.class2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_1929 {
-    private static boolean isPrime(int num){
-        for(int i = 2; i <= num/2; i++){
-            if(num % i == 0) return false;
+    private static boolean[] isPrime;
+    private static int START_NUM;
+    private static int END_NUM;
+
+    private static void makePrime() {
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i <= END_NUM; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= END_NUM; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int startNum = Integer.parseInt(st.nextToken());
-        int endNum = Integer.parseInt(st.nextToken());
-        for(int i = startNum; i <= endNum; i++){
-            if(isPrime(i)) sb.append(i).append("\n");
+
+        START_NUM = Integer.parseInt(st.nextToken());
+        END_NUM = Integer.parseInt(st.nextToken());
+
+        isPrime = new boolean[END_NUM + 1];
+
+        makePrime();
+
+        for (int i = START_NUM; i <= END_NUM; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
