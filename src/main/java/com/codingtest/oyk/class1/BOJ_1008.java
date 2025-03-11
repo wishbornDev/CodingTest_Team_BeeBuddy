723c48b643c7439478c655e54d33780932f4ef1b
oyk0510 <youkyungoh0510@naver.com>
2025-03-11 16:02:08 +0900

    1008번 A/B

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_1008.java
@@ -0,0 +1,21 @@
+package main.java.com.codingtest.oyk.class1;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double value;
+        value = a / (double) b;
+        System.out.println(value);
+//        double a = sc.nextDouble();
+//        double b = sc.nextDouble();
+//
+//        System.out.println(a/b);
+
+
+    }
+}
