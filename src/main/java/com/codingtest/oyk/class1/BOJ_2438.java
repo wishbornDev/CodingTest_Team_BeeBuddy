eb98f8e1d81dc28ee4d3db3059e8d1a9b277820e
oyk0510 <youkyungoh0510@naver.com>
2025-03-11 17:07:50 +0900

    2438번 별 찍기-1

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_2438.java
@@ -0,0 +1,18 @@
+package main.java.com.codingtest.oyk.class1;
+
+import java.util.Scanner;
+
+public class BOJ_2438 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
