667b4bb222aabba7c3315b53d44f8db9f31d0c12
oyk0510 <youkyungoh0510@naver.com>
2025-03-11 17:35:24 +0900

    2739번 구구단

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_2739.java
@@ -0,0 +1,19 @@
+package main.java.com.codingtest.oyk.class1;
+
+import java.util.Scanner;
+
+public class BOJ_2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = 0;
+
+        if(n >= 1 && n <= 9) {
+            for(int i = 1; i <= 9; i++) {
+                result = n * i;
+                System.out.println(n + " * " + i + " = " + result);
+            }
+        }
+    }
+}
