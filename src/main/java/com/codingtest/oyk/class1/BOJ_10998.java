f2214717379c5b77a1db322d438c8567820d0021
oyk0510 <youkyungoh0510@naver.com>
2025-03-12 16:41:35 +0900

    10998번 A*B

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_10998.java
@@ -0,0 +1,13 @@
+package main.java.com.codingtest.oyk.class1;
+
+import java.util.Scanner;
+
+public class BOJ_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
