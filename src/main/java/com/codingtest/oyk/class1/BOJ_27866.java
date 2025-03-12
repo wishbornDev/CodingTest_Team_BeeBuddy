563cf8f1f5cde7aea0ab620a1e8c4c982e3a153d
oyk0510 <youkyungoh0510@naver.com>
2025-03-12 17:37:34 +0900

    27866번 문자와 문자열

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_27866.java
@@ -0,0 +1,13 @@
+package main.java.com.codingtest.oyk.class1;
+
+import java.util.Scanner;
+
+public class BOJ_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+        System.out.println(s.charAt(n - 1));
+    }
+}
