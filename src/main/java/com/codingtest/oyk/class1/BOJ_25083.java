64926118f97465b146686894ce80bd9553de5cc3
oyk0510 <youkyungoh0510@naver.com>
2025-03-12 17:22:04 +0900

    25083번 새싹

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_25083.java
@@ -0,0 +1,20 @@
+package main.java.com.codingtest.oyk.class1;
+
+public class BOJ_25083 {
+    public static void main(String[] args) {
+        /*
+                 ,r'"7
+        r`-_   ,'  ,/
+         \. ". L_r'
+           `~\/
+              |
+              |
+         */
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
