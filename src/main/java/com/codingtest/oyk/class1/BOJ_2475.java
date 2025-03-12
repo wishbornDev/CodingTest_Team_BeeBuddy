bb9b413cb4f87ae54ca9cb32f8a5e592b92d8a90
oyk0510 <youkyungoh0510@naver.com>
2025-03-12 09:12:12 +0900

    2475번 검증수

--- src/main/java/com/codingtest/oyk/class1/BOJ_2475.java
+++ src/main/java/com/codingtest/oyk/class1/BOJ_2475.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class BOJ_2475 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
+        int result;
         int sum = 0;
         for(int i = 0; i < 5; i++) {
             int n = sc.nextInt();
-            int temp = 0;
+            int temp;
             temp = (n*n);
             sum += temp;
         }
