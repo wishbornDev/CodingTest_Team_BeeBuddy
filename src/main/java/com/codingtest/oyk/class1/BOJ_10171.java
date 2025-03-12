62371a7410fbaeb53c2b83b3403f83601ab283af
oyk0510 <youkyungoh0510@naver.com>
2025-03-12 09:22:29 +0900

    10171번 고양이

---
+++ src/main/java/com/codingtest/oyk/class1/BOJ_10171.java
@@ -0,0 +1,10 @@
+package main.java.com.codingtest.oyk.class1;
+
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
