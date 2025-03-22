2482ff1467567c744bbe8cb30aef6bcc66c54936
Cho-Hyun-Seung <toki0327@naver.com>
2025-03-22 13:50:09 +0900

    Hashing -100!

--- src/main/java/com/codingtest/chs/class2/BOJ_15829.java
+++ src/main/java/com/codingtest/chs/class2/BOJ_15829.java
@@ -1,23 +1,27 @@
 package main.java.com.codingtest.chs.class2;
 
 import java.io.*;
-import java.util.*;
+import java.math.BigInteger;
 
 public class BOJ_15829 {
-    private static final int M = 1234567891;
+    private static final BigInteger M = new BigInteger("1234567891");
+    private static final BigInteger R = new BigInteger("31");
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int length = Integer.parseInt(st.nextToken());    // 문자열 길이
-        String str = br.readLine();
-        // 입력 문자열
-        int hash = 0;    // 해시 값
+        int length = Integer.parseInt(br.readLine());  // 문자열 길이
+        String str = br.readLine();                    // 입력 문자열
 
-        for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-            hash += (ch - 'a' + 1)*((int)Math.pow(31,i)) % M;
+        BigInteger hash = BigInteger.ZERO;  // 해시값
+        BigInteger pow = BigInteger.ONE;
+
+        for (int i = 0; i < str.length(); i++) {
+            int value = str.charAt(i) - 'a' + 1;
+            BigInteger term = BigInteger.valueOf(value).multiply(pow).mod(M);
+            hash = hash.add(term).mod(M);
+            pow = pow.multiply(R).mod(M);
         }
+
         System.out.println(hash);
     }
 }
